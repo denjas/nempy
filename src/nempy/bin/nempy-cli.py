7a7be3739fa652ca302463480082003862dcf4c3
denja <ikuzen@gmail.com>
2021-07-18 00:08:46 +0300

    add: debug; monitoring

--- src/nempy/bin/nempy-cli.py
+++ src/nempy/bin/nempy-cli.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
+
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+import click
+from nempy.utils.sym_monitoring import main as monitoring_sym
+
+
 logging.getLogger('asyncio').setLevel(logging.ERROR)
 logging.getLogger('asyncio.coroutines').setLevel(logging.ERROR)
 logging.getLogger('websockets').setLevel(logging.ERROR)
 logging.getLogger('urllib3').setLevel(logging.ERROR)
 
 
-def main():
-    print('Hello NEMpy!')
+@click.group()
+@click.option('-d', '--debug', is_flag=True)
+def main(debug):
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+
+main.add_command(monitoring_sym, 'monitoring')
 
 
 if __name__ == '__main__':
