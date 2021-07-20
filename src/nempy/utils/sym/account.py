eb3cc3d6bca50774fdc022fa6073f880222bee14
denja <ikuzen@gmail.com>
2021-07-21 01:03:57 +0300

    account info refactoring

--- src/nempy/utils/sym/account.py
+++ src/nempy/utils/sym/account.py
@@ -1,7 +1,6 @@
 import os
 
 import click
-import stdiomask
 from nempy.wallet import Account, print_warning, DecoderStatus, Wallet, GenerationTypes
 
 
@@ -17,11 +16,11 @@ def main():
 @main.command('import')
 def import_account():
     wallet = Wallet()
-    account_path, name, network_type, bip32_coin_id = Account.init_general_params(wallet.default_profile.network_type)
+    account_path, name, bip32_coin_id = Account.init_general_params(wallet.default_profile.network_type)
     password = wallet.default_profile.check_pass(attempts=3)
     gen_type = Account.get_gen_type()
     if gen_type == GenerationTypes.MNEMONIC:
-        account = Account.account_by_mnemonic(network_type, bip32_coin_id)
+        account = Account.account_by_mnemonic(wallet.default_profile.network_type, bip32_coin_id)
     account = Account(account)
     account.name = name
     account.profile = wallet.default_profile.name
@@ -31,10 +30,10 @@ def import_account():
 @main.command('create')
 def create_account():
     wallet = Wallet()
-    account_path, name, network_type, bip32_coin_id = Account.init_general_params(wallet.default_profile.network_type)
+    account_path, name, bip32_coin_id = Account.init_general_params(wallet.default_profile.network_type)
     password = wallet.default_profile.check_pass(attempts=3)
     if password is not None:
-        account = Account.account_by_mnemonic(network_type, bip32_coin_id, is_generate=True)
+        account = Account.account_by_mnemonic(wallet.default_profile.network_type, bip32_coin_id, is_generate=True)
         account = Account(account)
         account.name = name
         account.profile = wallet.default_profile.name
@@ -52,19 +51,20 @@ def info(name, decode, is_list):
     account_path = Account.build_account_path(name)
     if not os.path.exists(account_path):
         print(f'The account named `{name}` does not exist')
-        exit(1)
+        wallet.default_profile.set_default_account()
     password = None
     if decode:
         print('Attention! Hide information received after entering a password from prying eyes')
         password = wallet.default_profile.check_pass(attempts=3)
         if password is None:
             exit(1)
-    if is_list:
-        accounts = Account.read_accounts(wallet.default_profile.name, password)
-    else:
-        accounts = [Account.read_account(account_path, password)]
-
-    for account in accounts:
+    accounts = wallet.default_profile.load_accounts(password)
+    if not accounts:
+        print(f'There are no accounts for the {wallet.default_profile.name} profile. To create an account, run the command: `nempy-cli.py account create`')
+        exit(1)
+    if not is_list:
+        accounts = {name: accounts.get(name, {})}
+    for account in accounts.values():
         if isinstance(account, DecoderStatus):
             exit(1)
         print(account)
