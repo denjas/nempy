22bf35387b6971810e30e1458949b71aeb9b8d30
denja <ikuzen@gmail.com>
2021-07-27 00:14:18 +0300

    add test err

--- tests/test_template.py
+++ tests/test_template.py
@@ -20,6 +20,6 @@ class TemplateTestCase(unittest.TestCase):
         pass
 
     def test_hello_world(self):
-        wallet = Wallet()
+        wallet = Wallet(skip_checks=True)
         result = init()
         self.assertEqual(result, 'Hello NEMpy!')
