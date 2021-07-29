fecad24ddd2c0c7870060fb59359752dac85e73c
denja <ikuzen@gmail.com>
2021-07-29 14:52:05 +0300

    test_account fix

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,27 +1,26 @@
-import os
-
-import pytest
-import stdiomask
 import tempfile
+from unittest.mock import patch
+
+import stdiomask
 from nempy.account import Account
 from nempy.sym.constants import NetworkType
 
 
-def test_account():
+@patch.object(Account, 'input_keyprint_entropy', return_value='test')
+def test_account(mock_keyprint_entropy):
     network_type = NetworkType.TEST_NET
     bip32_coin_id_test_net = 1
-    Account.input_keyprint_entropy = lambda: 'test'
-    Account.inquirer_account = lambda _: 'TD6HYHFAQPGN3QB5GH46I35RYWM5VMMBOW32RXQ'
-    account0 = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net,
-                                           is_generate=True)
 
-    stdiomask.getpass = lambda _: account0.mnemonic
-    account0_ = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net)
-    assert account0.address == account0_.address
+    with patch.object(Account, 'inquirer_account', return_value='TD6HYHFAQPGN3QB5GH46I35RYWM5VMMBOW32RXQ'):
+        account0 = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net,
+                                               is_generate=True)
+        with patch.object(stdiomask, 'getpass', return_value=account0.mnemonic):
+            account0_ = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net)
+            assert account0.address == account0_.address
 
-    Account.inquirer_account = lambda _: 'TBR5X6UG3ZT2IIOAP65Y7J7SLPN4UPARKH6HMUI'
-    account1 = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net,
-                                           is_generate=True)
+    with patch.object(Account, 'inquirer_account', return_value='TBR5X6UG3ZT2IIOAP65Y7J7SLPN4UPARKH6HMUI'):
+        account1 = Account.account_by_mnemonic(network_type=network_type, bip32_coin_id=bip32_coin_id_test_net,
+                                               is_generate=True)
 
     path = tempfile.NamedTemporaryFile().name
     password = 'pass'
