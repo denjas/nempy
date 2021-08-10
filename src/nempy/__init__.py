c857df678231888d2ac7f6a696bde9e521748776
denja <ikuzen@gmail.com>
2021-08-11 01:35:35 +0300

    added exceptions from documentation

--- src/nempy/__init__.py
+++ src/nempy/__init__.py
@@ -1,3 +1,10 @@
-
-__version__ = '0.0.2'
+__pdoc__ = {
+    "nempy.utils": False,
+    "nempy.bin": False,
+    "nempy.wallet": False,
+    "nempy.config": False,
+    "nempy.sym.network": True,
+    "nempy.sym.api": True,
+    "nempy.sym.ed25519": False,
+}
 
